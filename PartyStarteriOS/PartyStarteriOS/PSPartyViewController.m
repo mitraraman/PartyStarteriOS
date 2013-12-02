b4599f3ef832661b37933adc1e5a7905a7fcf75f
Stephanie <stephaniexyeung@gmail.com>
2013-12-02 18:19:06 -0500

    Decrement amount pitched in jank

--- PartyStarteriOS/PartyStarteriOS/PSPartyViewController.m
+++ PartyStarteriOS/PartyStarteriOS/PSPartyViewController.m
@@ -35,8 +35,14 @@
 - (void)viewDidLoad
 {
   [super viewDidLoad];
+  NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+  [dateFormat setDateFormat:@"EEEE MMMM d, YYYY"];
+  NSString *dateString = [dateFormat stringFromDate:self.party.date];
+  
   self.title = self.party.name;
   self.amountNeeded.text = [NSString stringWithFormat:@"%d", [self.party.totalCost intValue] - [self.party.fundedCost intValue]];
+  self.partyLocation.text = self.party.location;
+  self.partyHost.text = dateString;
 }
 
 - (void)didReceiveMemoryWarning
@@ -46,5 +52,8 @@
 }
 
 - (IBAction)pitchIn:(id)sender {
+  NSString* pitchedIn = self.amountPitchIn.text;
+  NSString* newAmountNeeded = [NSString stringWithFormat:@"%d", [self.amountNeeded.text intValue] - [pitchedIn intValue]];
+  self.amountNeeded.text = newAmountNeeded;
 }
 @end
